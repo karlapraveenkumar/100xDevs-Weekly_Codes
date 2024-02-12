bbdddeb4f2400b640fe855445d1c43d27b80e4cf
karlapraveenkumar <157881195+karlapraveenkumar@users.noreply.github.com>
2024-02-12 17:17:57 +0530

    Update useEffect.js

--- Week6/useEffect.js
+++ Week6/useEffect.js
@@ -32,3 +32,43 @@ function Todo({title, description}) {
 }
 
 export default App;
+
+
+// Using the axios
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React, {Fragment, useEffect, useState} from 'react'
+import axios from "axios"
+
+function App() {
+  const[todos, setTodos] = useState([]);
+  useEffect(()=>{
+    // We are now using axios to fetch data from backend
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(function(response){
+        setTodos(response.data.todos)
+      })
+  },[]);
+
+  return(
+    <div>
+      {todos.map(todo=>
+        <Todo key={todo.id} title={todo.title} description={todo.description}/>
+      )}
+    </div>
+  )
+
+}
+function Todo({title, description}){
+  return(
+    <div>
+      <h1>{title}</h1>
+      <h4>{description}</h4>
+    </div>
+  )
+}
+
+export default App
+
